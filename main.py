9ad4e7fcef37a3d578735cc9d90808c88f706a6f
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-08-12 15:35:08 +0530

    Update main.py to main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = Starlette(routes=[
     Route("/", homepage)
 ])
 
-if name == "main":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
